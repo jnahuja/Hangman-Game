14f10d2bd521ecd244c0abdd8dd358a2a0774f90
Jashan Ahuja <jashan.ahuja11@gmail.com>
2018-04-02 18:39:30 -0400

    Update Header Image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,6 +44,8 @@ $(document).ready(function () {
         guessesRemaining: 10,
         lettersWrong: [],
         lettersCorrect: [],
+        allLowercaseLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"],
+      
 
 
         selectWord: function () {
