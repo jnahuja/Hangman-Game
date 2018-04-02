5ca8f229cd649cfe097069d312680b435a04e5c7
Jashan Ahuja <jashan.ahuja11@gmail.com>
2018-04-02 09:07:07 -0400

    Hangman Game Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     // Define our initial variables, including "wins", "losses", "word array", and an empty variable for the user's letter guess
     var wins = 0;
     var losses = 0;
@@ -10,6 +10,20 @@ $(document).ready(function () {
     var currentWordDiv = main.find("#currentWord");
     var lettersGuessedDiv = main.find("#lettersGuessed");
     var guessesRemainingDiv = main.find("#guessesRemaining");
+    var winsDiv = main.find("#winsPanel");
+    var lossesDiv = main.find("#lossesPanel");
+
+    // Gets Link for Theme Song
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/captainplanet24.mp3");
+
+    // Theme Button
+    $("#musicControls").on("click", ".theme-button", function() {
+      audioElement.play();
+    }).on("click", ".pause-button", function() {
+      audioElement.pause();
+    });
+
 
     // Define an object for the setup of the game. The object must include:
     // (1) A function for selecting the word from the wordBank
@@ -35,7 +49,7 @@ $(document).ready(function () {
             this.wordBankIndex = Math.floor(Math.random() * wordBank.length);
             console.log(this.wordBankIndex);
             this.mainWordString = wordBank[this.wordBankIndex];
-            for (i=0; i<this.mainWordString.length; i++) {
+            for (i = 0; i < this.mainWordString.length; i++) {
                 this.mainWord[i] = this.mainWordString.charAt(i);
             }
         },
@@ -45,7 +59,7 @@ $(document).ready(function () {
         // (2) Create element to hold word
         // (3) Populate that element with a word from our "word" array
         // (4) Make the word a random color from our "text color" array
-        buildWord: function() {
+        buildWord: function () {
             for (var i = 0; i < this.mainWord.length; i++) {
                 // 1. Create a variable named "letterDiv" equal to $("<div>") to hold our new div and div properties
                 var letterDiv = $("<div>");
@@ -58,23 +72,36 @@ $(document).ready(function () {
                 // letterDiv.css('color', blue);
                 // 3. Append our new div to "colorPicker", which will fill up with a new div each time this "for" loop executes
                 currentWordDiv.append(letterDiv);
-            }    
+            }
         },
 
-        initializeGuessesRemaining: function() {
+        initializeGameVariables: function () {
+            this.guessesRemaining = 10;
+            this.lettersCorrect = [];
+            this.lettersWrong = [];
+            this.mainWord = [];
+            this.mainWordString = "";
+            this.wordBankIndex = 0;
+        },
+
+        initializeGuessesRemainingDiv: function () {
             guessesRemainingDiv.text(this.guessesRemaining);
+            winsDiv.text(wins);
+            lossesDiv.text(losses);
         },
 
-        initialize: function() {
+        initialize: function () {
+            this.initializeGameVariables();
             this.selectWord();
             this.buildWord();
-            this.initializeGuessesRemaining();
+            this.initializeGuessesRemainingDiv();
+
         }
 
         // secondBuildWord: function () {
         //     wordHolder = document.getElementById('currentWord');
         //     correct = document.createElement('ul');
-        
+
         //     for (var i = 0; i < this.mainWord.length; i++) {
         //       correct.setAttribute('id', 'my-word');
         //       guess = document.createElement('li');
@@ -86,7 +113,7 @@ $(document).ready(function () {
         //     //   } else {
         //     //     guess.innerHTML = "_";
         //     //   }
-        
+
         //     //   geusses.push(guess);
         //       wordHolder.appendChild(correct);
         //       correct.appendChild(guess);
@@ -101,104 +128,142 @@ $(document).ready(function () {
     // gamePlay.secondBuildWord();
 
     // When the user presses a key, it will run the following function...
-    document.onkeyup = function(event) {
+    document.onkeyup = function (event) {
         // Determines which key is pressed and stores it in "userGuess"
         userGuess = event.key;
         console.log(userGuess);
         console.log(gamePlay.mainWord);
         var letterCorrect = false;
+        var newGuess = true;
 
-        for (i=0; i<gamePlay.mainWord.length; i++) {
-            console.log(gamePlay.mainWord[i]);
-            if (userGuess==gamePlay.mainWord[i]) {
-                letterCorrect = true;
-                var currentDiv = main.find(("#Index" + i));
-                console.log(currentDiv);
-                currentDiv.addClass("oldLetterDiv");
-                currentDiv.removeClass("newLetterDiv");
-                currentDiv.text(userGuess);
+        // Evaluate whether a guess is a new guess or not
+        for (j = 0; j < gamePlay.lettersCorrect.length; j++) {
+            if (userGuess == gamePlay.lettersCorrect[j]) {
+                newGuess = false;
             }
         }
-        if (letterCorrect==false) {
-            gamePlay.lettersWrong.push(userGuess);
-            console.log(gamePlay.lettersWrong);
-            // 1. Create a variable named "letterDiv" equal to $("<div>") to hold our new div and div properties
-            var letterDiv = $("<div>");
-            // 2. Then give letterDiv the relevant class, attributes, and text content
-            letterDiv.addClass("oldLetterDiv");
-            // letterDiv.attr("id", ("Index" + i));
-            // letterDiv.attr("data-letter", ("Index" + i)); // this.mainWord[i]
-            letterDiv.text(userGuess);
-            // letterDiv.text(this.mainWord[i]);
-            // letterDiv.css('color', blue);
-            // 3. Append our new div to "colorPicker", which will fill up with a new div each time this "for" loop executes
-            lettersGuessedDiv.append(letterDiv);
+        for (k = 0; k < gamePlay.lettersWrong.length; k++) {
+            if (userGuess == gamePlay.lettersWrong[k]) {
+                newGuess = false;
+            }
+        }
+        if (newGuess) {
+            for (i = 0; i < gamePlay.mainWord.length; i++) {
+                console.log(gamePlay.mainWord[i]);
+                if (userGuess == gamePlay.mainWord[i]) {
+                    letterCorrect = true;
+                    var currentDiv = main.find(("#Index" + i));
+                    console.log(currentDiv);
+                    currentDiv.addClass("oldLetterDiv");
+                    currentDiv.removeClass("newLetterDiv");
+                    currentDiv.text(userGuess);
+
+                    gamePlay.lettersCorrect.push(userGuess);
+                }
+            }
+            if (letterCorrect == false) {
+                gamePlay.lettersWrong.push(userGuess);
+                console.log(gamePlay.lettersWrong);
+                // 1. Create a variable named "letterDiv" equal to $("<div>") to hold our new div and div properties
+                var letterDiv = $("<div>");
+                // 2. Then give letterDiv the relevant class, attributes, and text content
+                letterDiv.addClass("oldLetterDiv");
+                // letterDiv.attr("id", ("Index" + i));
+                // letterDiv.attr("data-letter", ("Index" + i)); // this.mainWord[i]
+                letterDiv.text(userGuess);
+                // letterDiv.text(this.mainWord[i]);
+                // letterDiv.css('color', blue);
+                // 3. Append our new div to "colorPicker", which will fill up with a new div each time this "for" loop executes
+                lettersGuessedDiv.append(letterDiv);
+                gamePlay.lettersWrong.push(userGuess);
+                gamePlay.guessesRemaining = gamePlay.guessesRemaining - 1;
+            }
         }
 
-        gamePlay.guessesRemaining = gamePlay.guessesRemaining - 1;
+        // gamePlay.guessesRemaining = gamePlay.guessesRemaining - 1;
         guessesRemainingDiv.text(gamePlay.guessesRemaining);
 
-        
+        var restartGame = false;
+        setTimeout(function () {
+            if (gamePlay.lettersCorrect.length == gamePlay.mainWord.length) {
+                wins = wins + 1;
+                alert("You Win!");
+                restartGame = confirm("Would you like to play another game?");
+            }
+            if (gamePlay.guessesRemaining == 0) {
+                losses = losses + 1;
+                alert("You Lose!");
+                restartGame = confirm("Would you like to play another game?");
+            }
+            if (restartGame) {
+                currentWordDiv.empty();
+                lettersGuessedDiv.empty();
+                gamePlay.initialize();
+            }
+        }, 1000);
+
+        // winsDiv.text(wins);
+        // lossesDiv.text(losses);
 
     }
 
 
 
-            // var excessObject {
+    // var excessObject {
 
-            //     makeGuess: function (x) {
-            //         this.guessesRemaining = this.guessesRemaining - 1;
-            //         //if (x
-            //     },
+    //     makeGuess: function (x) {
+    //         this.guessesRemaining = this.guessesRemaining - 1;
+    //         //if (x
+    //     },
 
-            //     guessReset: function () {
-            //         this.guessesRemaining = 10;
-            //     },
+    //     guessReset: function () {
+    //         this.guessesRemaining = 10;
+    //     },
 
-            //     wordBankIndexReset: function () {
-            //         this.wordBankIndex = 0;
-            //     },
+    //     wordBankIndexReset: function () {
+    //         this.wordBankIndex = 0;
+    //     },
 
-            // }
-
-
-
-            // Function that adds a word to an ever-growing wordBank array
-            // function addWord(x) {
-                // wordBank.push(x);
-            // }
-
-            // Need a validation to prevent duplicate words from being added to the wordBank array
-            // Need a function to remove words from the wordBank
-
-            // Function that updates the wins on screen
-            // function updateWins() {
-            //     document.querySelector("#wins").innerHTML = "Wins: " + wins;
-            // }
-
-            // function renderQuestion() {
-            //     // If there are still more questions, render the next one.
-            //     if (questionIndex <= (questionsArray.length - 1)) {
-            //         document.querySelector("#question").innerHTML = questionsArray[questionIndex][0];
-            //     }
-            //     // If there aren't, render the end game screen.
-            //     else {
-            //         document.querySelector("#question").innerHTML = "Game Over!";
-            //         document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questionsArray.length;
-            //     }
-            // }
+    // }
 
 
-            // // MAIN PROCESS
-            // // ==============================================================================
 
-            // // Calling functions to start the game.
-            // renderQuestion();
-            // updateScore();
+    // Function that adds a word to an ever-growing wordBank array
+    // function addWord(x) {
+    // wordBank.push(x);
+    // }
+
+    // Need a validation to prevent duplicate words from being added to the wordBank array
+    // Need a function to remove words from the wordBank
+
+    // Function that updates the wins on screen
+    // function updateWins() {
+    //     document.querySelector("#wins").innerHTML = "Wins: " + wins;
+    // }
+
+    // function renderQuestion() {
+    //     // If there are still more questions, render the next one.
+    //     if (questionIndex <= (questionsArray.length - 1)) {
+    //         document.querySelector("#question").innerHTML = questionsArray[questionIndex][0];
+    //     }
+    //     // If there aren't, render the end game screen.
+    //     else {
+    //         document.querySelector("#question").innerHTML = "Game Over!";
+    //         document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questionsArray.length;
+    //     }
+    // }
 
 
-            // var userGuessHTML = document.getElementById("userGuess");
+    // // MAIN PROCESS
+    // // ==============================================================================
 
-            
+    // // Calling functions to start the game.
+    // renderQuestion();
+    // updateScore();
+
+
+    // var userGuessHTML = document.getElementById("userGuess");
+
+
 
 });
